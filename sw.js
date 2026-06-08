4fc8cfba81dff7a6810db1061c3cf9db0f1abd69
pescados0de0abvssal-wq <pescados0de0abvssal@gmail.com>
2026-06-08 22:36:23 +0900

    feat: add free-text sticky memo with title/body, separate from highlight memos

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'learning-notes-v4';
+const CACHE_NAME = 'learning-notes-v5';
 const ASSETS = ['./', './index.html', './manifest.json'];
 
 self.addEventListener('install', e => {
